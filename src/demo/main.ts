f5b85f33bcccd49580325f49b10499c2efa26033
miles.majefski <miles.majefski@realpage.com>
2026-03-25 11:52:54 -0500

    Project builds and runs, but is empty

---
+++ src/demo/main.ts
@@ -0,0 +1,5 @@
+const project_def = "Lsys3d Yay!!!";
+
+console.log("----------------------------------");
+console.log(`Project Definition: ${project_def}`);
+console.log("----------------------------------");
